78dabc28dda080afd51f17850336589d15f740be
Clint-Mathews <mathewsclint28@gmail.com>
2022-02-27 20:06:17 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTToken_App.Controllers
+{
+    [Route("api/general")]
+    [Authorize]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok(new List<int> { 1,2,3,4});
+        }
+    }
+}
---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTToken_App.Controllers
+{
+    [Route("api/auth")]
+    public class SecurityController : ControllerBase
+    {
+        private ILogger<SecurityController> _logger;
+        private IConfiguration _configuration;
+        public SecurityController(ILogger<SecurityController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult register()
+        {
+            return Ok(GenerateJWTToken("Clint"));
+        }
+
+        // Only called once authentication is done
+        private string GenerateJWTToken(string APIKey)
+        {
+            var a = _configuration["SECERT_KEY"];
+            // Using secret key to generate security key.
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SECERT_KEY"]));
+            // Algorithm and securityKey to create the signing credentials
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            // Claims 
+            var claims = new[]
+            {
+                new Claim("User","Clint"),
+                new Claim(JwtRegisteredClaimNames.UniqueName, APIKey)
+            };
+            // Token Creation
+            var token = new JwtSecurityToken(
+                "Clint",
+                "Clint",
+                claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
